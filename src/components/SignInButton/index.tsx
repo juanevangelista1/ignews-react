1d0be1493fb1f3f8edfc0aeee6a4211894cb7b84
juanevangelista1 <juan.evangelista.nascimentoo@gmail.com>
2022-04-10 21:01:59 -0300

    Link com o FaunaDB e pagamento com o Stripe

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,15 +1,19 @@
 import styles from './styles.module.scss'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-import { signIn, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 export function SignInButton() {
   const { data: session } = useSession()
 
   return session ? (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Juan Evangelista
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
@@ -23,3 +27,6 @@ export function SignInButton() {
     </button>
   ) //Aqui foi feito uma condicional para mudar o botão se o usuário estiver loggado ou não
 }
+
+// SignIn é a função para a autenticação do usuário .
+//Usessesion é um hook para retornar se o usuário está logado ou não.
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,17 +1,57 @@
+import {query as q} from 'faunadb'
+
+import {fauna } from '../../../services/fauna'
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
 export default NextAuth({
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      
       authorization: {
         params: {
-          scope: 'read:user',
+          scope: 'read:user'
         },
       },
     }),
-    // ...add more providers here
   ],
+  
+  callbacks: {
+    async signIn({user: User, account: Account, profile: Profile})
+ {
+  const { email } = User
+  try {
+    await fauna.query(
+      q.If(
+        q.Not(
+          q.Exists(
+            q.Match(
+              q.Index('user_by_email'),
+              q.Casefold(email)
+            )
+          )
+        ),
+        q.Create(
+          q.Collection('users'),
+          { data: { email } }
+        ),
+        q.Get(
+          q.Match(
+            q.Index('user_by_email'),
+            q.Casefold(email)
+          )
+        )
+      )
+    )
+        return true
+      }
+      catch{
+        return false;
+      }
+
+    }
+  }
 })
